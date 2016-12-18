d8a66592534763336d2c3a6f91972074ee62ac27
Cass May <ingeniousmammal@gmail.com>
2016-12-18 00:33:35 +0000

    Switch to using easymesh lib
    Seems that the frequent disconnections were just an artifact of the WiFi library I was using. This works fine, and is much less laggy

--- ESP/ESPClient/ESPClient.ino
+++ ESP/ESPClient/ESPClient.ino
@@ -1,62 +1,65 @@
-#include <QueueArray.h>
-#include <ESP8266WiFi.h>
-#include<Wire.h>
+#include <easyMesh.h>
 
-const char* ssid     = "BopIt";
-const char* password = "upVsg49hh0CjNo80";
+#define   LED             2       // GPIO number of connected LED
 
-const char* host = "192.168.4.1";
+#define   MESH_PREFIX     "BopIt"
+#define   MESH_PASSWORD   "upVsg49hh0CjNo80"
+#define   MESH_PORT       5555
 
-// create a queue of characters.
-volatile QueueArray <char> queue;
+easyMesh  mesh;
 
-WiFiClient client;
+uint32_t sendMessageTime = 0;
+int ledState = 0;
+String msg = "";
+char msgChar = 'a';
+long unsigned int lastDebounceTime = 0;
+int minDebounceInterval = 20;
 
 void buttonISR () {
-    //Serial.println("Button pushed");
-    queue.push('X');
+  if (millis() < lastDebounceTime + minDebounceInterval) {
+    return;
+  }
+  lastDebounceTime = millis();
+  msg += msgChar++;
+  if (msgChar == 'z') {
+    msgChar = 'a';
+  }
 }
-
 void setup() {
-  digitalWrite(2,HIGH);
   Serial.begin(115200);
-  pinMode(2, OUTPUT);
-  pinMode(5, INPUT);
+    
+  pinMode( LED, OUTPUT );
+  digitalWrite( LED, HIGH);
+
+//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT );
+  mesh.setReceiveCallback( &receivedCallback );
+  mesh.setNewConnectionCallback( &newConnectionCallback );
   attachInterrupt(5, buttonISR, RISING);
 
-  // set the printer of the queue.
-  queue.setPrinter (Serial);
-  delay(100);
-  
-  Serial.println("Serial begins");
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("WIFI OK");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
-  Serial.println("Connected to Wifi");
-  digitalWrite(2,LOW);
+  randomSeed( analogRead( A0 ) );
 }
 
-long cntAccel=0,cntSend=0;
 
 void loop() {
-  cntSend++;
-  cntAccel++;
-  if(!client.connected())
-  {
-    client.connect(host,80);
+  mesh.update();
+
+  // if the time is ripe, send everyone a message!
+  if ( msg.length() != 0 ){
+    Serial.println(msg);
+    mesh.sendBroadcast( msg );
+    msg = "";
   }
-  if(cntSend==800)
-  {
-      while (!queue.isEmpty()) {
-          const char val = queue.pop();
-          Serial.println(val);
-          client.print(val);
-          client.flush();
-      }
-      cntSend=0;
-  }
+}
+
+void receivedCallback( uint32_t from, String &msg ) {
+  digitalWrite( LED, ledState++%2);
+  Serial.print(msg);
+}
+
+void newConnectionCallback( bool adopt ) {
+  Serial.printf("startHere: New Connection, adopt=%d\n", adopt);
+  digitalWrite( LED, LOW);
 }
--- ESP/ESPServer/ESPServer.ino
+++ ESP/ESPServer/ESPServer.ino
@@ -1,60 +1,42 @@
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-#include <WiFiServer.h>
+#include <easyMesh.h>
 
-const char *ssid = "BopIt";
-const char *password = "upVsg49hh0CjNo80";
+#define   LED             2       // GPIO number of connected LED
 
-WiFiServer server(80);
+#define   MESH_PREFIX     "BopIt"
+#define   MESH_PASSWORD   "upVsg49hh0CjNo80"
+#define   MESH_PORT       5555
+
+easyMesh  mesh;
+int ledState = 0;
 
 void setup() {
-    Serial.begin(115200);
-    pinMode(2, OUTPUT);
-    digitalWrite(2,HIGH);
-    WiFi.softAP(ssid, password);
+  Serial.begin(115200);
+    
+  pinMode( LED, OUTPUT );
+  digitalWrite( LED, HIGH);
 
-    IPAddress myIP = WiFi.softAPIP();
-    Serial.print("Server IP = ");
-    Serial.println(myIP);
-    server.begin();
+//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT );
+  mesh.setReceiveCallback( &receivedCallback );
+  mesh.setNewConnectionCallback( &newConnectionCallback );
+
+  randomSeed( analogRead( A0 ) );
 }
+
 
 void loop() {
-    WiFiClient client = server.available();
-  
-    if (client) {
-        digitalWrite(2,LOW);
-        Serial.println("Client connected.");
-    
-        while (client.connected()) {
-            while (client.available()) {
-                char command = client.read();
-                Serial.println(command);
-            }
-        }
-        Serial.println("Client disconnected.");
-        client.stop();
-        digitalWrite(2,HIGH);
-    }
+  mesh.update();
 }
 
-     
-        /*while(!client.available())
-        {
-            delay(1);
-        }
-        Serial.println("Client available");
-        String s=client.read('X');
-        if(s.length()!=0)
-        {
-            Serial.println("Nonzero length message");
-            Serial.println(s);
-        }
-        else
-        {
-            Serial.println("Zero length message");
-        }
-        client.flush();
-    }
-}*/
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.print(msg);
+  
+  digitalWrite( LED, ledState++%2);
+}
+
+void newConnectionCallback( bool adopt ) {
+  Serial.printf("startHere: New Connection, adopt=%d\n", adopt);
+  digitalWrite( LED, LOW);
+}
